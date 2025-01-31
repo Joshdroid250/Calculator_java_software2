407854ce72b8f506214acf92d4712728ff38329e
MARCELO-M-C <marmoli2005@gmail.com>
2025-01-31 16:45:25 -0600

    Added_substraction_and_negate_Tests

--- src/test/java/CalculatorLogicTest.java
+++ src/test/java/CalculatorLogicTest.java
@@ -31,5 +31,21 @@ public class CalculatorLogicTest {
         calculator.setOperator('/');
         assertThrows(ArithmeticException.class, () -> calculator.calculate(), "Division by zero should throw an exception");
     }
+
+    @Test
+    public void testSubtraction() {
+        CalculatorLogic calculator = new CalculatorLogic();
+        calculator.setNum1(10);
+        calculator.setNum2(4);
+        calculator.setOperator('-');
+        assertEquals(6, calculator.calculate(), 0.0001);
+    }
+
+    @Test
+    public void testNegate() {
+        CalculatorLogic calculator = new CalculatorLogic();
+        assertEquals(-5, calculator.negate(5), 0.0001);
+        assertEquals(7, calculator.negate(-7), 0.0001);
+    }
 }
 
